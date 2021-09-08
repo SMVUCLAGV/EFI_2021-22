13c75fddfe905a0f85eb93f31e6d096646a4be74
ashi <arhisonb@gmail.com>
2021-09-07 21:00:50 -0700

    updates to ser

--- teensy_ser/ser.c
+++ teensy_ser/ser.c
@@ -9,24 +9,37 @@
 
 #define BAUDRATE 115200
 
-// Private Prototypes
+//************ Private Declarations ************ 
 static volatile struct ser_flags ser_f;
-static volatile uint8_t rcvbuffer[2][SER_BUFFER_BYTE_SIZE];
-static volatile uint32_t sendbuffer_numbytes, rcvbuffer_numbytes;
 
+// tx variabls
+static volatile uint
+
+// read buffer related
+static volatile uint8_t readbuffer[2][SER_BUFFER_BYTE_SIZE];
+static volatile uint32_t readbuffer_len;
+static volatile uint32_t readbuffer_curr; // the buffer currently written to
+
+// Functions
 static void ser_handleuart();
+static uint8_t* ser_txfifoload(uint8_t* str, uint32_t num); // returns pointer 
+                                                            // to after num chars
 static uint32_t ser_txfifolevel();
 static uint32_t ser_rxfifolevel();
 
-// Public Definitions
+//************ Public Definitions ************ 
 void ser_init() {
-  // Set up flags
+  // init local variables
   ser_f.WIP = 0;
   ser_f.WS = 0;
   ser_f.ORB = 0;
-
   ser_f.TC = 1;
-  
+
+  readbuffer_len = 0;
+  readbuffer_curr = 0;
+
+  // **** UART SETUP ****
+
   // Set up FIFOs and waterlevel
   _RB_CLR(REG_C2, TE);  // enable transmitter
   _RB_CLR(REG_C2, RE);  // enable receiver
@@ -59,9 +72,22 @@ void ser_init() {
 // musn't change while this happens. If a string is already being sent,
 // that transmission will stop in favor of new one.
 uint32_t ser_write(const uint8_t* towrite, uint32_t numbytes) {
-  // Reset flags to appropriate values
-  //
+  ser_f.WS = 0;
+
+  // Check if string is currently being written. If so, cancel old write.
+  if (ser_f.WIP) {
+    // Clear TXFIFO
+    _RB_CLR(REG_C2, TE);  // turn off transmitter
+    _RB_SET(REG_CFIFO, TXFLUSH); // flush tx fifo
+    _RB_CLR(REG_CFIFO, TXFLUSH); 
+    _RB_SET(REG_C2, TE);  // enable transmitter
+
+    // Set Write Skipped Flag
+    ser_f.WS = 1;
+  }
+
   // Kick off writting
+  ser_f.WIP = 1;
 }
 
 
@@ -80,6 +106,13 @@ void ser_status((const struct ser_flags*)* fptrptr) {
 // Private Definitions
 void uart0_status_irs(void) {
   ser_handleuart();
+}
+
+static uint8_t* ser_txfifoload(uint8_t* str, uint32_t num) {
+  for (int i = 0; i < num; i++)
+    _RW_SET(REG_D, 7, 0, str[i]);
+
+  return str[num];
 }
 
 static void ser_handleuart() {
